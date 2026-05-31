e011ace99b7af14956c7df53aac552f2c3f0360b
Shardul <mygithub.903@gmail.com>
2026-05-31 12:26:19 -0400

    fixed errors in the scripts.js ifle

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const calculate = () => {
-  let p = document.getElementById("principle").value;
+  let p = document.getElementById("principal").value;
   let r = document.getElementById("rate").value;
   let t = document.getElementById("time").value;
   let simpleInterest = (p * r * t) / 100;
@@ -7,7 +7,7 @@ const calculate = () => {
 
   let result = document.getElementById("result");
 
-  result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
+  result.innerHTML = `<div>Principal Amount: <span>${parseFloat(p).toFixed(2)}</span></div>
   <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
   <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
 };
